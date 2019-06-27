092d9015dfbd093c2879052dd3dccacd0f1f9edf
msidqi <mouadmr89@gmail.com>
2019-06-27 03:09:21 +0100

    moving light source with num_keys

--- src/ft_events_keyboard.c
+++ src/ft_events_keyboard.c
@@ -66,12 +66,25 @@ int     key_press(int key_code, t_data *data)
     ft_refresh_image(data);
     if (key_code == LEFT_ARROW)
         data->cam.position.v[X] += 0.5;
-    else if (key_code == RIGHT_ARROW)
+    if (key_code == RIGHT_ARROW)
         data->cam.position.v[X] -= 0.5;
-    else if (key_code == UP_ARROW)
+    if (key_code == UP_ARROW)
         data->cam.position.v[Z] -= 0.5;
-    else if (key_code == DOWN_ARROW)
+    if (key_code == DOWN_ARROW)
         data->cam.position.v[Z] += 0.5;
+
+    if (key_code == NUM_LOCK_8)
+        data->light.origin.v[Z] -= 0.2;
+    if (key_code == NUM_LOCK_5)
+        data->light.origin.v[Z] += 0.2;
+    if (key_code == NUM_LOCK_4)
+        data->light.origin.v[X] += 0.2;
+    if (key_code == NUM_LOCK_6)
+        data->light.origin.v[X] -= 0.2;
+    if (key_code == NUM_LOCK_7)
+        data->light.origin.v[Y] -= 0.2;
+    if (key_code == NUM_LOCK_9)
+        data->light.origin.v[Y] += 0.2;
     t_vector4 cam_pos = ft_create_vector4(data->cam.position.v[X], data->cam.position.v[Y], data->cam.position.v[Z], 1);
 	t_vector4 look_at_pos = ft_create_vector4(0, 0, -1, 1);
 
